af09dc95f1fcad3d5447c7e531cfa5d01c455815
Arianna Ravera <ariannaravera22@gmail.com>
2023-09-29 09:19:39 +0200

    Added files and code

--- codeimpulse.py
+++ codeimpulse.py
@@ -6,6 +6,8 @@ import tifffile
 import cv2
 from pystackreg import StackReg
 from random import randrange
+from mozaic import create_mozaic
+import random
 
 
 def count_images_per_folder(directory, folders):
@@ -51,8 +53,9 @@ def generate_stack(results_directory, directory):
                 names.append(image_name)
                 n+=1
     file = open('names.txt','w')
-    for item in names:
+    for item in names[:-1]:
         file.write(item+"\n")
+    file.write(names[-1])
     file.close()
     try:
         tifffile.imwrite(results_directory+'image_stack.tif', image_stack, imagej=True, metadata={'axes': 'TYX'})
@@ -163,37 +166,124 @@ def equalize_image(image_src, gray_scale=False):
         image_eq = enhance_contrast(image_matrix=image_src)
     return image_eq
 
-def mosaic(results_directory):
+def timelines(results_directory):
     print('-------------------------------')
-    print('Complete mosaic generation...')
-    image_res = np.zeros((720, 1280, 3), dtype='uint8')
-    column = 0
+    print('Complete timelines generation...')
     image_stack_aligned = tifffile.imread(results_directory+'image_stack_aligned_rgb.tif')
+    image_res = np.zeros((image_stack_aligned.shape[1], image_stack_aligned.shape[2], 3), dtype='uint8')
     
     names = open("names.txt", "r").read()
     dates = [n.split('-')[2] for n in names.split("\n")]
     dates = [d[6:8]+'/'+d[4:6]+'/'+d[0:4] for d in dates]
-    
+
+    column = 0
     for i in range(256):
         image = image_stack_aligned[i+50]
         image = equalize_image(image)
         # 5 pixel columns for each image in analysis
-        for j in range(5):
-            image_col = image[:,column+j, :].copy()
-            image_res[:,column+j, :] = image_col
-            
+        image_col = image[:,column:column+5, :].copy()
+        image_res[:,column:column+5, :] = image_col
         column += 5
     # Save result
-    tifffile.imwrite(results_directory+'mosaic_image.tif', image_res)
+    tifffile.imwrite(results_directory+'timelines_image.tif', image_res)
 
     plt.figure()
     plt.imshow(image_res)
     plt.xticks(np.arange(1, 1281, 75), dates[50:306:15], rotation='vertical')
     plt.yticks([])
-    plt.savefig(results_directory+'mosaic_image_dates.jpg', bbox_inches='tight', dpi=200)
+    plt.savefig(results_directory+'timelines_with_dates.jpg', bbox_inches='tight', dpi=200)
 
     print('Image saved!')
     print('-------------------------------')
+
+def timelines_not_aligned(results_directory):
+    print('-------------------------------')
+    print('Complete timelines not aligned generation...')
+    #image_stack= tifffile.imread('/Volumes/RECHERCHE/CTR/CI/DCSR/abarenco/impulse/D2c/rawpix/Arianna_results/image_stack.tif')
+    image_stack_R = tifffile.imread('/Users/aravera/Documents/Impulse_Figerletempsquipasse/image_stack_R.tif')
+    image_stack_G = tifffile.imread('/Users/aravera/Documents/Impulse_Figerletempsquipasse/image_stack_G.tif')
+    image_stack_B = tifffile.imread('/Users/aravera/Documents/Impulse_Figerletempsquipasse/image_stack_B.tif')
+    #image_res = np.zeros((image_stack.shape[1], image_stack.shape[2]), dtype='uint8')
+    image_res_R = np.zeros((image_stack_R.shape[1], image_stack_R.shape[2]), dtype='uint8')
+    image_res_G = np.zeros((image_stack_R.shape[1], image_stack_R.shape[2]), dtype='uint8')
+    image_res_B = np.zeros((image_stack_R.shape[1], image_stack_R.shape[2]), dtype='uint8')
+
+    names = open("names.txt", "r").read()
+    dates = [n.split('-')[2] for n in names.split("\n")]
+    dates = [d[6:8]+'/'+d[4:6]+'/'+d[0:4] for d in dates]
+
+    column = 0
+    for i in range(256):
+        #image = image_stack[i+50]
+        image_R = image_stack_R[i+50]
+        image_G = image_stack_G[i+50]
+        image_B = image_stack_B[i+50]
+        #image = equalize_image(image)
+        # 5 pixel columns for each image in analysis
+        """image_col = image[:,column:column+5].copy()
+        image_res[:,column:column+5] = image_col"""
+        
+        image_col = image_R[:,column:column+5].copy()
+        image_res_R[:,column:column+5] = image_col
+        
+        image_col = image_G[:,column:column+5].copy()
+        image_res_G[:,column:column+5] = image_col
+        
+        image_col = image_B[:,column:column+5].copy()
+        image_res_B[:,column:column+5] = image_col
+        column += 5
+    # Save result
+    tifffile.imwrite(results_directory+'timelines_notaligned_R.tif', image_res_R)
+    tifffile.imwrite(results_directory+'timelines_notaligned_G.tif', image_res_G)
+    tifffile.imwrite(results_directory+'timelines_notaligned_B.tif', image_res_B)
+
+    """plt.figure()
+    plt.imshow(image_res)
+    plt.xticks(np.arange(1, 1281, 75), dates[50:306:15], rotation='vertical')
+    plt.yticks([])
+    plt.savefig(results_directory+'timelines_notaligned_with_dates.jpg', bbox_inches='tight', dpi=200)"""
+
+    print('Image saved!')
+    print('-------------------------------')
+
+def trees_timelines(results_directory):
+    print('-------------------------------')
+    print('Trees timelines generation...')
+    image_stack_aligned = tifffile.imread('/Users/aravera/Documents/Impulse_Figerletempsquipasse/results/image_stack_aligned_rgb.tif')
+    #results_directory+'image_stack_aligned_rgb.tif')
+    image_stack_aligned = image_stack_aligned[:, 350:,:600, :]
+    image_res = np.zeros((image_stack_aligned.shape[1], 768, 3), dtype='uint8')
+    
+    names = open("names.txt", "r").read()
+    dates = [n.split('-')[2] for n in names.split("\n")]
+    dates = [d[6:8]+'/'+d[4:6]+'/'+d[0:4] for d in dates]
+
+    column = 0
+    column2 = 0
+    for i in range(256):
+        image = image_stack_aligned[i+50]
+        # 5 pixel columns for each image in analysis
+        image_col = image[:,column2:column2+3, :].copy()
+        image_res[:,column:column+3, :] = image_col
+        column += 3
+        column2 += 2
+    
+    image_res1 = cv2.pyrUp(image_res)
+    image_res = cv2.pyrUp(image_res1)
+    image_res = cv2.blur(image_res, (3, 3)) 
+    # Save result
+    tifffile.imwrite(results_directory+'timelines_trees.tif', image_res, imagej=True)
+
+    plt.figure()
+    plt.imshow(image_res)
+    plt.xticks(np.arange(1, image_res.shape[1]+2, 192), dates[50:306:15][:-1], rotation=40)
+    plt.yticks([])
+    plt.xticks(fontsize=5)
+    plt.savefig(results_directory+'timelines_trees_with_dates.jpg', bbox_inches='tight', dpi=200)
+
+    print('Image saved!')
+    print('-------------------------------')
+
 
 def chessboard(results_directory):
     print('-------------------------------')
@@ -204,32 +294,198 @@ def chessboard(results_directory):
     """names = open("names.txt", "r").read()
     dates = [n.split('-')[2] for n in names.split("\n")]
     dates = [d[6:8]+'/'+d[4:6]+'/'+d[0:4] for d in dates]"""
-    steps = [10, 20, 40, 80]
+    steps = [20, 40, 80]
     for step in steps:
         for column in range(0, 1280, step):
             # 5 pixel columns for each image in analysis
             for row in range(0, 720, step):
-                if column < 500: frame = randrange(50, 150)
-                if column > 500 and column < 700: frame = randrange(150, 300)
-                if column >700: frame = randrange(300, 468)
+                if column < 700: frame = randrange(50, 150)
+                if column > 700: frame = randrange(150, 468)
                 image = equalize_image(image_stack_aligned[frame])
                 image_res[row:row+step,column:column+step, :] = image[row:row+step,column:column+step, :].copy()
         # Save result
-        tifffile.imwrite(results_directory+'chessboard_step'+str(step)+'_image.tif', image_res, imagej=True)
+        tifffile.imwrite(results_directory+'new_chessboard_step'+str(step)+'_image.tif', image_res, imagej=True)
 
     print('Image saved!')
     print('-------------------------------')
 
 
-def main():
-    # TODO: 
-    # new alignment -> I tried but result seems not better at all, try again with another corner f the image 
-    # checkboard -> did it
-    # crop the trees
-    # crop the votex
+def generate_daily_stack(results_directory, directory, date):
+    print('-------------------------------')
+    print('Creation of the stack...')
+    names = []
+    image_stack = np.zeros((481, 720, 1280), dtype='uint8')
+    image_stack_R = np.zeros((481, 720, 1280), dtype='uint8')
+    image_stack_G = np.zeros((481, 720, 1280), dtype='uint8')
+    image_stack_B  = np.zeros((481, 720, 1280), dtype='uint8')
     
+    images = os.listdir(directory)
+    images.sort()
+    n = 0
+    
+    for image_name in images[::2]:
+        image = np.asarray(Image.open(os.path.join(directory, image_name)))
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        image_stack[n] = gray
+        image_stack_R[n] = image[:,:,0]
+        image_stack_G[n] = image[:,:,1]
+        image_stack_B[n] = image[:,:,2]
+        names.append(image_name)
+        n+=1
+
+    file = open('daily_names_'+date+'.txt','w')
+    for item in names[:-1]:
+        file.write(item+"\n")
+    file.write(names[-1])
+    file.close()
+    try:
+        tifffile.imwrite(results_directory+'daily_'+date+'_stack.tif', image_stack, imagej=True, metadata={'axes': 'TYX'})
+        tifffile.imwrite(results_directory+'daily_'+date+'_stack_R.tif', image_stack_R, imagej=True, metadata={'axes': 'TYX'})
+        tifffile.imwrite(results_directory+'daily_'+date+'_stack_G.tif', image_stack_G, imagej=True, metadata={'axes': 'TYX'})
+        tifffile.imwrite(results_directory+'daily_'+date+'_stack_B.tif', image_stack_B, imagej=True, metadata={'axes': 'TYX'})
+    except:
+        tifffile.imwrite(results_directory+'daily_'+date+'_stack.tif', image_stack)
+        tifffile.imwrite(results_directory+'daily_'+date+'_stack_R.tif', image_stack_R)
+        tifffile.imwrite(results_directory+'daily_'+date+'_stack_G.tif', image_stack_G)
+        tifffile.imwrite(results_directory+'daily_'+date+'_stack_B.tif', image_stack_B)
+    
+    print('Stacks saved')
+    print('-------------------------------')
+    return image_stack
+ 
+def daily_timelines(results_directory):
+    """print('-------------------------------')
+    print('Daily timelines generation...')
+    image_stack_aligned = tifffile.imread(os.path.join(results_directory,'daily_20200319_stack_rgb.tif'))
+    # (481, 720, 1280, 3)
+    image_res = np.zeros((image_stack_aligned.shape[1], image_stack_aligned.shape[2], image_stack_aligned.shape[3]), dtype='uint8')
+
+    n_columns_per_image = 6
+    column = 0
+    for i in range(0, image_stack_aligned.shape[0], 2):
+        image = image_stack_aligned[i]
+        image = equalize_image(image)
+        if column+6 < image_stack_aligned.shape[2]:
+            # n_columns_per_image pixel columns for each image in analysis
+            image_col = image[:,column:column+n_columns_per_image, :].copy()
+            image_res[:,column:column+n_columns_per_image, :] = image_col
+            column += n_columns_per_image
+        else:
+            n_columns_per_image = image_stack_aligned.shape[2] - column
+            image_col = image[:,column:column+n_columns_per_image, :].copy()
+            image_res[:,column:column+n_columns_per_image, :] = image_col
+            column += n_columns_per_image
+    # Save result
+    tifffile.imwrite(results_directory+'timelines_daily.tif', image_res)
+
+    try:
+        names = open("daily_names_20200319.txt", "r").read()
+        #hour format = 0500030986
+        hours = [n.split('-')[3].split('.')[0][:4] for n in names.split("\n")]
+        hours = [h[:2]+':'+h[2:] for h in hours]
+        plt.figure()
+        plt.imshow(image_res)
+        plt.xticks(np.arange(1, 1281, 75), hours[0::28], rotation='vertical')
+        plt.yticks([])
+        plt.savefig(results_directory+'timelines_daily_with_dates.jpg', bbox_inches='tight', dpi=200)
+    except Exception as e:
+        print(e)"""
+    
+    print('-------------------------------')
+    print('Daily timelines generation...')
+    image_stack_aligned = tifffile.imread(os.path.join(results_directory,'daily_20200319_stack_rgb.tif'))
+    # (481, 720, 1280, 3)
+    image_res = np.zeros((image_stack_aligned.shape[1], image_stack_aligned.shape[2], image_stack_aligned.shape[3]), dtype='uint8')
+
+    # > 06 and < 0640 + 18-19
+    n_columns_per_image = 6
+    column = 0
+
+    names = open("daily_names_20200319.txt", "r").read()
+    #hour format = 0500030986
+    hours = [n.split('-')[3].split('.')[0][:4] for n in names.split("\n")]
+    
+    skip = 1
+    for i in range(len(hours)):
+        hour = hours[i]
+        if (str(hour[:4]) > '0530' and str(hour[:4]) < '0710') or (str(hour[:4]) > '1700' and str(hour[:4]) < '2000'):
+            # 46 images = 276 columns
+            image = image_stack_aligned[i]
+            image = equalize_image(image)
+            if column+6 < image_stack_aligned.shape[2]:
+                # n_columns_per_image pixel columns for each image in analysis
+                image_col = image[:,column:column+n_columns_per_image, :].copy()
+                image_res[:,column:column+n_columns_per_image, :] = image_col
+                column += n_columns_per_image
+            else:
+                n_columns_per_image = image_stack_aligned.shape[2] - column
+                image_col = image[:,column:column+n_columns_per_image, :].copy()
+                image_res[:,column:column+n_columns_per_image, :] = image_col
+                column += n_columns_per_image
+        else:
+            # altre 201 immagini su 435
+            if skip == 3:
+                skip = 0
+                image = image_stack_aligned[i]
+                image = equalize_image(image)
+                if column+6 < image_stack_aligned.shape[2]:
+                    # n_columns_per_image pixel columns for each image in analysis
+                    image_col = image[:,column:column+n_columns_per_image, :].copy()
+                    image_res[:,column:column+n_columns_per_image, :] = image_col
+                    column += n_columns_per_image
+                else:
+                    n_columns_per_image = image_stack_aligned.shape[2] - column
+                    image_col = image[:,column:column+n_columns_per_image, :].copy()
+                    image_res[:,column:column+n_columns_per_image, :] = image_col
+                    column += n_columns_per_image
+            else:
+                skip += 1
+                
+    # Save result
+    tifffile.imwrite(results_directory+'timelines_daily.tif', image_res)
+
+    try:
+        names = open("daily_names_20200319.txt", "r").read()
+        #hour format = 0500030986
+        hours = [n.split('-')[3].split('.')[0][:4] for n in names.split("\n")]
+        hours = [h[:2]+':'+h[2:] for h in hours]
+        plt.figure()
+        plt.imshow(image_res)
+        plt.xticks(np.arange(1, 1281, 75), hours[0::28], rotation='vertical')
+        plt.yticks([])
+        plt.savefig(results_directory+'timelines_daily_with_dates.jpg', bbox_inches='tight', dpi=200)
+    except Exception as e:
+        print(e)
+
+    print('Image saved!')
+    print('-------------------------------')
+
+
+def mozaic(directory, results_directory, imagenames_list, num_bins_x, num_bins_y, date_list):
+    dimensions = [720, 1280, 3] # [height, width, channels]   
+    result_image = np.zeros((dimensions))
+
+    mozaic_data = create_mozaic(dimensions[1], dimensions[0], num_bins_x, num_bins_y, min_date, max_date, date_list)
+    
+    for el in mozaic_data:
+        x_pos, y_pos, date = el
+        image_name = None
+        for n in imagenames_list:
+            if date.replace('-','') in n: #eg. n = cam-cub-20171006-1200552463.jpg
+                image_name = n 
+        if image_name:
+            image = np.asarray(Image.open(os.path.join(directory, image_name)))
+            # insert pixel x,y in result image
+            result_image[x_pos, y_pos, :] = image[x_pos, y_pos, :]
+        else:
+            ('WARNING! Image with date '+str(date)+' not found!')
+    
+    tifffile.imwrite(results_directory+'mozaic.tif', result_image, imagej=True)
+
+
+def main():    
     directory = '/Volumes/RECHERCHE/CTR/CI/DCSR/abarenco/impulse/D2c/rawpix/summary_1200/'
-    results_directory = '/Volumes/RECHERCHE/CTR/CI/DCSR/abarenco/impulse/D2c/rawpix/Arianna_results/'
+    results_directory = '/Users/aravera/Documents/Impulse_Figerletempsquipasse/'#'/Volumes/RECHERCHE/CTR/CI/DCSR/abarenco/impulse/D2c/rawpix/Arianna_results/'
     folders = os.listdir(directory)
     folders.sort()
     if not os.path.exists(results_directory+''):
@@ -238,8 +494,36 @@ def main():
     #count_images_per_folder(directory, folders)
     #generate_stack(results_directory, directory)
     #align_stack(results_directory, results_directory+'transformationMatrix.txt')
-    #mosaic(results_directory)
-    chessboard(results_directory)
+    #timelines(results_directory)
+    #chessboard(results_directory)
+    #trees_timelines(results_directory)
+    #generate_daily_stack(results_directory, '/Volumes/RECHERCHE/CTR/CI/DCSR/abarenco/impulse/D2c/rawpix/vortex/2020-03-19', '20200319')
+    #timelines_not_aligned(results_directory)
+    #daily_timelines(results_directory)
+
+    # List of available images
+    imagenames_list = open("names.txt", "r").read()
+    # Corresponding list of dates in format yyy-mm-dd
+    imagedates_list = [n.split('-')[2] for n in imagenames_list.split("\n")]
+    imagedates_list = [d[0:4]+'-'+d[4:6]+'-'+d[6:8] for d in imagedates_list] 
+    # Number of horizontal "boxes" in the output mozaic image
+    num_bins_x = 300 
+    # Number of vertical "boxes" in the output mozaic image
+    num_bins_y = 100  
+    
+    # In date_list (x, y) positions must be values within [0, 1] x [0, 1] and they should NOT be collinear, e.g.
+    # BAD: date_list = [(0.1, 0.1, "2021-09-26"), (0.5, 0.5, "2022-05-02"), (0.9, 0.9, "2022-03-27")]
+    # GOOD: date_list = [(0.15, 0.1, "2021-09-26"), (0.3, 0.5, "2022-05-02"), (0.7, 0.9, "2022-03-27")]
+    
+    random_dates = random.sample(imagedates_list, 4) # choosing random dates as starting points
+    
+    date_list = [(0.01, 0.01, random_dates[0]),
+                 (0.01, 0.99, random_dates[1]),
+                 (0.99,0.01, random_dates[2]),
+                 (0.99, 0.99, random_dates[3])]
+    # Create and save mozaic image
+    mozaic(directory, results_directory, imagenames_list, num_bins_x, num_bins_y, date_list)
+
     
 	
 if __name__ == "__main__":
---
+++ mozaic.py
@@ -0,0 +1,126 @@
+from datetime import datetime, timedelta
+from dateutil.parser import parse
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+def date_to_seconds(date_str):
+    try:
+        date_obj = parse(date_str)
+        return (date_obj - datetime.utcfromtimestamp(0)).total_seconds()
+    except:
+        return np.nan
+
+def seconds_to_date(seconds):
+    if np.isnan(seconds):
+        return "NaN"
+    return (datetime.utcfromtimestamp(0) + timedelta(seconds=seconds)).strftime("%Y-%m-%d")
+
+def create_mozaic(image_width, image_height, num_bins_x, num_bins_y, min_date, max_date, date_list):
+
+    mozaic_width = image_width / num_bins_x
+    mozaic_height = image_height / num_bins_y
+
+    if mozaic_width % 1 == 0 and mozaic_height % 1 == 0:
+        mozaic_width = int(mozaic_width)
+        mozaic_height = int(mozaic_height)
+    else:
+        print("Please make sure that the ratios (image_width / num_bins_x) and (image_height / num_bins_y) are integers")
+        sys.exit()
+
+    max_date = date_to_seconds(max_date)
+    min_date = date_to_seconds(min_date)
+
+    x_bins = np.linspace(0, 1, num_bins_x + 1)
+    y_bins = np.linspace(0, 1, num_bins_y + 1)
+
+    mozaic = np.full((num_bins_x, num_bins_y), np.nan)
+
+    for x, y, date in date_list:
+        i = int(x * num_bins_x)
+        j = int(y * num_bins_y)
+        k = date_to_seconds(date)
+        mozaic[i, j] = k
+
+    x_indices, y_indices = np.meshgrid(np.arange(num_bins_x), np.arange(num_bins_y))
+    x_indices = x_indices.flatten()
+    y_indices = y_indices.flatten()
+    mozaic_values = np.transpose(mozaic).flatten()
+
+    # Valid indices contain float values given as input
+    valid_indices = ~np.isnan(mozaic_values)
+    valid_x_indices = x_indices[valid_indices]
+    valid_y_indices = y_indices[valid_indices]
+    valid_dates = mozaic_values[valid_indices]
+
+    # Interpolate values for all grid points with a cubic spline
+    interpolated_dates = griddata(
+        (valid_x_indices, valid_y_indices),
+        valid_dates,
+        (x_indices, y_indices),
+        method='cubic',
+        fill_value=np.nan
+    )
+
+    # The cubic method may leave some NaN values, so we use a simpler method to fill in these remaining NaNs
+    if(np.isnan(interpolated_dates).any()):
+
+        # Valid indices contain float values given as input + spline interpolation
+        valid_indices = ~np.isnan(interpolated_dates)
+        valid_x_indices = x_indices[valid_indices]
+        valid_y_indices = y_indices[valid_indices]
+        valid_dates = interpolated_dates[valid_indices]
+
+        # Interpolate the remaining NaNs values with the nearest method
+        interpolated_dates = griddata(
+            (valid_x_indices, valid_y_indices),
+         valid_dates,
+            (x_indices, y_indices),
+            method='nearest',
+            fill_value=np.nan
+        )
+
+    # Clip the interpolated dates to the specified range
+    interpolated_dates = np.clip(interpolated_dates, min_date, max_date)
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    c = ax.pcolormesh(x_bins, y_bins, interpolated_dates.reshape(num_bins_y, num_bins_x), shading='auto')
+    """plt.colorbar(c, label='Date (seconds)')
+    plt.xlabel('X Bins')
+    plt.ylabel('Y Bins')
+    plt.title('Mozaic Interpolation')
+    plt.savefig('mozaic.png')
+    plt.close(fig)"""
+
+    result = []
+    for i, j in zip(x_indices, y_indices):
+        value = seconds_to_date(interpolated_dates[i + j * num_bins_x])
+        for x_delta in range(mozaic_width):
+            for y_delta in range(mozaic_height):
+                x_pixel = i * mozaic_width + x_delta
+                y_pixel = j * mozaic_height + y_delta
+                result.append((x_pixel, y_pixel, value))
+
+    sorted_result = sorted(result, key = lambda x: (x[1], x[0]))
+
+    return sorted_result
+
+# How to use it #
+image_width = 10 # the number of pixels for the width of the output image
+image_height = 10 # the number of pixels for the height of the output image
+num_bins_x = 2 # the number of horizontal "boxes" in the output mozaic image
+num_bins_y = 5  # the number of vertical "boxes" in the output mozaic image
+min_date = "2021-01-01" # the smallest acceptable date
+max_date = "2023-12-31" # the largest acceptable date
+
+# the positions (x, y) must be values within [0, 1] x [0, 1] and they should NOT be collinear, e.g.
+# BAD: date_list = [(0.1, 0.1, "2021-09-26"), (0.5, 0.5, "2022-05-02"), (0.9, 0.9, "2022-03-27")]
+# GOOD: date_list = [(0.15, 0.1, "2021-09-26"), (0.3, 0.5, "2022-05-02"), (0.7, 0.9, "2022-03-27")]
+
+#date_list = [(0.1, 0.1, "2021-09-26"), (0.1, 0.9, "2023-09-27"), (0.5, 0.5, "2022-05-02"),  (0.9, 0.1, "2021-03-27"), (0.9, 0.9, "2022-03-27")]
+date_list = [(0.01, 0.01, "2021-09-26"),(0.01, 0.99, "2021-09-26"), (0.99,0.01, "2022-03-27"), (0.99, 0.99, "2022-03-27")]
+
+mozaic = create_mozaic(image_width, image_height, num_bins_x, num_bins_y, min_date, max_date, date_list)
+
+print(mozaic)
